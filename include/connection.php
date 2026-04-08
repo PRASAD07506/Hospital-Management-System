e729e361e55a228013905146e0ce87799543db90
PRASAD07506 <kumarprasadmohanty@gmail.com>
2026-04-08 10:23:19 +0530

    your changes

--- include/connection.php
+++ include/connection.php
@@ -1,4 +1,14 @@
-<?php 
-	$connect = mysqli_connect("localhost","root","","hms");
+<?php
+
+$connect = mysqli_connect(
+    "sql206.infinityfree.com",   // your host
+    "if0_41575875",              // your DB username
+    "Prasad@2004",             // your DB password
+    "if0_41575875_hms"           // your DB name
+);
+
+if (!$connect) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
 ?>
-	
